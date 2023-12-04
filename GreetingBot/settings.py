f17b6a1e9eaf8c8bdebc6fc338ed4a1a3aecdd53
fan9704 <cxz123499@gmail.com>
2023-12-04 19:32:55 +0800

    chore: Apply PostgresSQL Engine to settings.py

--- GreetingBot/settings.py
+++ GreetingBot/settings.py
@@ -9,24 +9,22 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mds(tvj3l4(l%%4p*axzue+i3g0hq(+$orrdi4jxgz*aw&$^3m'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GreetingBot.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB", "greet"),
+        'TEST': {
+            'NAME': os.getenv("POSTGRES_TEST_DB", "greettest")
+        },
+        'USER': os.getenv("POSTGRES_USER", "test"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "123456"),
+        'HOST': os.getenv("POSTGRES_DB_URL", "127.0.0.1"),
+        'PORT': os.getenv("POSTGRES_DB_PORT", "5432")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
