eb539fcad2ea2c93deda639acbae7d04414837b9
fan9704 <cxz123499@gmail.com>
2023-12-06 00:10:28 +0800

    feat: Complete XML Message and Register on Route

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,8 @@ from api.views.message import SimpleMessageAPIView
 from api.views.difference_gender import DifferenceGenderMessageAPIView
 from api.views.over_49 import Over49APIView
 from api.views.fullname import FullnameAPIView
-from api.views.nosql_message import  NoSQLMessageAPIView
+from api.views.nosql_message import NoSQLMessageAPIView
+from api.views.xml_message import XMLSimpleMessageAPIView
 
 urlpatterns = [
     # LINE Bot
@@ -15,5 +16,6 @@ urlpatterns = [
     path('over49/', Over49APIView.as_view(), name="Over49-API"),
     path('fullname/', FullnameAPIView.as_view(), name="Fullname-API"),
     path('nosql-message/', NoSQLMessageAPIView.as_view(), name="NoSQL-Message-API"),
+    path('xml-message/', XMLSimpleMessageAPIView.as_view(), name="XML-Message-API"),
 
 ]
