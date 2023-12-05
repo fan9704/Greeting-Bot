866461c489b099255198fa8ab9ba8bf99c244f78
fan9704 <cxz123499@gmail.com>
2023-12-05 23:59:38 +0800

    feat: Complete NoSQL Simple Message and Register Route

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from api.views.message import SimpleMessageAPIView
 from api.views.difference_gender import DifferenceGenderMessageAPIView
 from api.views.over_49 import Over49APIView
 from api.views.fullname import FullnameAPIView
+from api.views.nosql_message import  NoSQLMessageAPIView
 
 urlpatterns = [
     # LINE Bot
@@ -13,5 +14,6 @@ urlpatterns = [
     path('difference-gender/', DifferenceGenderMessageAPIView.as_view(), name="Difference-Gender-API"),
     path('over49/', Over49APIView.as_view(), name="Over49-API"),
     path('fullname/', FullnameAPIView.as_view(), name="Fullname-API"),
+    path('nosql-message/', NoSQLMessageAPIView.as_view(), name="NoSQL-Message-API"),
 
 ]
