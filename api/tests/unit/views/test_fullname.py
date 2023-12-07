af3de91bd172a411ce13489a2f84c1c87e9e8038
fan9704 <cxz123499@gmail.com>
2023-12-08 00:06:45 +0800

    fix: Fullname Send Message without name and Separate ORM into UserService

--- api/tests/unit/views/test_fullname.py
+++ api/tests/unit/views/test_fullname.py
@@ -16,40 +16,59 @@ class FullnameAPIViewTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.route = '/api/fullname/'
+        self.today = datetime.datetime.today().date()
 
-    @patch('api.models.User.objects.get_or_create')
-    def test_fullname(self, mock_get_or_create):
+    @patch('api.service.user.UserService.get_birthday_user_queryset')
+    def test_fullname(self, mock_get_birthday_user_queryset):
         data = {
-            "username": "Tom",
             "message": "Happy Birthday!"
         }
         request = self.factory.post(path=self.route, data=data, format='json')
-        mock_get_or_create.return_value = User.objects.create(
-            last_name="Cat",
-            first_name="Tom",
-            date_of_birth=datetime.date(1900, 9, 1),
-            email="u1@gmail.com"
-        ), None
+        mock_get_birthday_user_queryset.return_value = [
+            User.objects.create(
+                last_name="Cat",
+                first_name="Tom",
+                date_of_birth=datetime.date(
+                    1900,
+                    self.today.month,
+                    self.today.day
+                ),
+                email="u1@gmail.com"
+        ),
+            User.objects.create(
+                last_name="Mouse",
+                first_name="Jelly",
+                date_of_birth=datetime.date(
+                    2023,
+                    self.today.month,
+                    self.today.day
+                ),
+                email="u2@gmail.com"
+            ),
+        ]
 
         view = FullnameAPIView.as_view()
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['status'], 'success')
-        self.assertTrue(response.data['message'], 'Happy birthday, dear Tom,Cat')
+        self.assertEqual(response.data[0]['status'], 'success')
+        self.assertEqual(response.data[1]['status'], 'success')
+        self.assertTrue(response.data[0]['message'], 'Happy birthday, dear Tom, Cat!')
+        self.assertTrue(response.data[1]['message'], 'Happy birthday, dear Jelly, Mouse!')
         logger.info("Complete Fullname View Test")
 
-    def test_fullname_with_invalid_data(self):
+    @patch('api.service.user.UserService.get_birthday_user_queryset')
+    def test_fullname_with_invalid_data(self, mock_get_birthday_user_queryset):
         data = {
-            "username": "",
             "message": ""
         }
         request = self.factory.post(path=self.route, data=data, format='json')
+        mock_get_birthday_user_queryset.return_value = []
 
         view = FullnameAPIView.as_view()
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['status'], 'fail')
-        self.assertTrue(response.data['message'], 'message invalid')
+        self.assertEqual(response.data[0]['status'], 'fail')
+        self.assertTrue(response.data[0]['message'], 'message invalid')
         logger.info("Complete Fullname View Test with Invalid Data")
