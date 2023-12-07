082b6d91be2c1554f9c5a94a436a8619f513ff14
fan9704 <cxz123499@gmail.com>
2023-12-08 00:15:00 +0800

    fix: Fix NoSQL Message Spec to get All User Info

--- api/views/nosql_message.py
+++ api/views/nosql_message.py
@@ -1,55 +1,46 @@
-import datetime
-from mongoengine import connect
-
 import logging
 
 from rest_framework.exceptions import ValidationError
 
-from api.serializers.user import SimpleMessageSerializer
-from api.documents import User
+from api.serializers.user import MessageSerializer
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from api.service.mongo.user import UserService
+
 logger = logging.getLogger(__name__)
-connect('greet')
 
 
 class NoSQLMessageAPIView(APIView):
+    repository = UserService
+
     @swagger_auto_schema(
         operation_summary='NoSQL Message',
         operation_description='Reply username whole name use NoSQL',
-        request_body=SimpleMessageSerializer
+        request_body=MessageSerializer
     )
     def post(self, request, *args, **kwargs):
         try:
-            serializer = SimpleMessageSerializer(data=request.data)
-            serializer.is_valid()
-
-            user = User.objects(last_name=request.data.get("last_name"))
-            fullname = f'{user.last_name},{user.first_name}'
-            if "happy birthday" in serializer.data["message"].lower():
-                return Response({
-                    "status": "success",
-                    "message": f'Happy birthday, dear {fullname}',
-                }, status=status.HTTP_200_OK)
+            if "happy birthday" in request.data.get("message", "").lower():
+                birthday_user_list = self.repository.get_birthday_user_queryset()
+                response = []
+                for user in birthday_user_list:
+                    single_response = {
+                        "status": "success",
+                        "message": f'Happy birthday, dear {user.last_name}',
+                    }
+                    response.append(single_response)
+                return Response(response, status=status.HTTP_200_OK)
             else:
-                return Response({
+                response = [{
                     "status": "fail",
-                    "message": "message invalid"
-                }, status=status.HTTP_400_BAD_REQUEST)
+                    "message": 'message invalid',
+                }]
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
         except ValidationError:
             return Response({
                 "status": "fail",
                 "message": "data invalid"
             }, status=status.HTTP_400_BAD_REQUEST)
-    def patch(self,request):
-        user = User(
-            email=request.data.get("email"),
-            last_name=request.data.get("last_name"),
-            first_name=request.data.get("first_name"),
-            gender=request.data.get("gender"),
-            date_of_birth=request.data.get("date_of_birth")
-                    )
-        user.save()
