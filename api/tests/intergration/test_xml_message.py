d20efe43b6f02f1767f0c28bc16e467fe1f70370
fan9704 <cxz123499@gmail.com>
2023-12-08 00:27:33 +0800

    fix: according view's modification fix unittest and integration test and update new view

--- api/tests/intergration/test_xml_message.py
+++ api/tests/intergration/test_xml_message.py
@@ -1,9 +1,11 @@
+import datetime
 import logging
 
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 
+from api.models import User
 from api.views.xml_message import XMLSimpleMessageAPIView
 
 logger = logging.getLogger(__name__)
@@ -12,13 +14,33 @@ logger = logging.getLogger(__name__)
 class XMLSimpleMessageAPIViewTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.today = datetime.datetime.today().date()
+        self.u1 = User.objects.create(
+            first_name="Cat",
+            last_name="Tom",
+            date_of_birth=datetime.date(
+                1900,
+                self.today.month,
+                self.today.day
+            ),
+            email="u1@gmail.com"
+        )
+        self.u2 = User.objects.create(
+            first_name="Mouse",
+            last_name="Jelly",
+            date_of_birth=datetime.date(
+                2023,
+                self.today.month,
+                self.today.day
+            ),
+            email="u2@gmail.com"
+        )
         self.route = '/api/xml-message/'
 
     def test_XML_simple_message(self):
         data = """
         <SimpleMessage>
             <message>Happy Birthday</message>
-            <username>Tom</username>
         </SimpleMessage>
         """
         request = self.factory.post(path=self.route, data=data,  content_type='application/xml')
@@ -27,14 +49,14 @@ class XMLSimpleMessageAPIViewTestCase(TestCase):
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['status'], 'success')
-        self.assertTrue(response.data['message'], 'Happy birthday, dear Tom')
-        logger.info("Complete Simple Message View Test with Happy Birthday")
+        self.assertEqual(response.data[0]['status'], 'success')
+        self.assertTrue(response.data[0]['message'], 'Happy birthday, dear Tom')
+        logger.info("Complete XML Message View Test with Happy Birthday")
 
+    def test_XML_message_invalid_data(self):
         data = """
         <SimpleMessage>
             <message></message>
-            <username></username>
         </SimpleMessage>
         """
         request = self.factory.post(path=self.route, data=data,  content_type='application/xml')
@@ -43,6 +65,6 @@ class XMLSimpleMessageAPIViewTestCase(TestCase):
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['status'], 'fail')
-        self.assertTrue(response.data['message'], 'message invalid')
-        logger.info("Complete Simple Message View Test with invalid data")
+        self.assertEqual(response.data[0]['status'], 'fail')
+        self.assertTrue(response.data[0]['message'], 'message invalid')
+        logger.info("Complete XML Message View Test with invalid data")
--- api/tests/unit/views/test_xml_message.py
+++ api/tests/unit/views/test_xml_message.py
@@ -1,9 +1,12 @@
+import datetime
 import logging
+from unittest.mock import patch
 
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 
+from api.models import User
 from api.views.xml_message import XMLSimpleMessageAPIView
 
 logger = logging.getLogger(__name__)
@@ -14,27 +17,42 @@ class XMLSimpleMessageAPIViewTestCase(TestCase):
         self.factory = APIRequestFactory()
         self.route = '/api/xml-message/'
 
-    def test_XML_simple_message(self):
+    @patch('api.service.user.UserService.get_birthday_user_queryset')
+    def test_XML_simple_message(self, mock_get_birthday_user_queryset):
         data = """
         <SimpleMessage>
             <message>Happy Birthday</message>
-            <username>Tom</username>
         </SimpleMessage>
         """
         request = self.factory.post(path=self.route, data=data,  content_type='application/xml')
+        mock_get_birthday_user_queryset.return_value = [
+            User.objects.create(
+                last_name="Cat",
+                first_name="Tom",
+                date_of_birth=datetime.datetime.today(),
+                email="u1@gmail.com"
+        ),
+            User.objects.create(
+                last_name="Mouse",
+                first_name="Jelly",
+                date_of_birth=datetime.datetime.today(),
+                email="u2@gmail.com"
+            ),
+        ]
 
         view = XMLSimpleMessageAPIView.as_view()
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['status'], 'success')
-        self.assertTrue(response.data['message'], 'Happy birthday, dear Tom')
-        logger.info("Complete Simple Message View Test with Happy Birthday")
+        self.assertEqual(response.data[0]['status'], 'success')
+        self.assertTrue(response.data[0]['message'], 'Happy birthday, dear Tom')
+        logger.info("Complete XML Message View Test with Happy Birthday")
 
+    @patch('api.service.user.UserService.get_birthday_user_queryset')
+    def test_XML_message_invalid_data(self, mock_get_birthday_user_queryset):
         data = """
         <SimpleMessage>
             <message></message>
-            <username></username>
         </SimpleMessage>
         """
         request = self.factory.post(path=self.route, data=data,  content_type='application/xml')
@@ -43,6 +61,6 @@ class XMLSimpleMessageAPIViewTestCase(TestCase):
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['status'], 'fail')
-        self.assertTrue(response.data['message'], 'message invalid')
-        logger.info("Complete Simple Message View Test with invalid data")
+        self.assertEqual(response.data[0]['status'], 'fail')
+        self.assertTrue(response.data[0]['message'], 'message invalid')
+        logger.info("Complete XML Message View Test with invalid data")
