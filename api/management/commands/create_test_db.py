e83a98906dd414af0664c69d065aa5e43c0c51fe
fan9704 <cxz123499@gmail.com>
2023-12-15 12:27:26 +0800

    test: Test Database Connection according Database Engine to choose test container database

--- api/management/commands/create_test_db.py
+++ api/management/commands/create_test_db.py
@@ -1,22 +1,40 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 from django.db.utils import ProgrammingError
+from django.conf import settings
+from pymongo import MongoClient
+import os
 
 
 class Command(BaseCommand):
     help = 'Create a PostgreSQL database named "test" for testing'
 
     def handle(self, *args, **options):
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT datname FROM pg_database WHERE datname = 'test';")
-                database_exists = cursor.fetchone()
-        except ProgrammingError:
-            database_exists = False
+        if settings.DATABASES['default'].get("ENGINE") == "djongo":
+            client = MongoClient(host=os.getenv("MONGO_DB_URL"), port=int(os.getenv("MONGO_DB_PORT", 27017)))
+            try:
+                # Check if the 'test' database already exists
+                database_exists = client['test'].name in client.list_database_names()
+            except:
+                database_exists = False
 
-        if not database_exists:
-            with connection.cursor() as cursor:
-                cursor.execute('CREATE DATABASE TEST;')
-            self.stdout.write(self.style.SUCCESS('Database "test" created successfully.'))
-        else:
-            self.stdout.write(self.style.SUCCESS('Database "test" already exists.'))
+            if not database_exists:
+                # Create the 'test' database
+                client['test']
+                self.stdout.write(self.style.SUCCESS('Database "test" created successfully.'))
+            else:
+                self.stdout.write(self.style.SUCCESS('Database "test" already exists.'))
+        elif settings.DATABASES['default'].get("ENGINE") == "django.db.backends.postgresql":
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT datname FROM pg_database WHERE datname = 'test';")
+                    database_exists = cursor.fetchone()
+            except ProgrammingError:
+                database_exists = False
+
+            if not database_exists:
+                with connection.cursor() as cursor:
+                    cursor.execute('CREATE DATABASE TEST;')
+                self.stdout.write(self.style.SUCCESS('Database "test" created successfully.'))
+            else:
+                self.stdout.write(self.style.SUCCESS('Database "test" already exists.'))
--- api/tests/unit/commands/test_create_test_db.py
+++ api/tests/unit/commands/test_create_test_db.py
@@ -1,21 +1,41 @@
+import logging
+import os
+
 from django.core.management import call_command
 from django.test import TransactionTestCase
 from django.db import connections
+from django.conf import settings
 from psycopg2 import sql
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
 
 
 class CreateTestDatabaseTestCase(TransactionTestCase):
     def test_create_test_database(self):
         call_command('create_test_db')
+        if settings.DATABASES['default'].get("ENGINE") == "djongo":
+            client = MongoClient(host=os.getenv("MONGO_DB_URL"), port=int(os.getenv("MONGO_DB_PORT", 27017)))
+            database = client['test']
 
-        with connections['default'].cursor() as cursor:
-            query = sql.SQL("SELECT datname FROM pg_database WHERE datname = %s;")
-            cursor.execute(query, ['test'])
-            database_exists = cursor.fetchone()
+            # Check if database exists
+            database_exists = database.name in client.list_database_names()
 
-        self.assertIsNotNone(database_exists)
+            self.assertIsNotNone(database_exists)
+        elif settings.DATABASES['default'].get("ENGINE") == "django.db.backends.postgresql":
+
+            with connections['default'].cursor() as cursor:
+                query = sql.SQL("SELECT datname FROM pg_database WHERE datname = %s;")
+                cursor.execute(query, ['test'])
+                database_exists = cursor.fetchone()
+
+            self.assertIsNotNone(database_exists)
 
     def tearDown(self):
-        with connections['default'].cursor() as cursor:
-            query = sql.SQL("DROP DATABASE IF EXISTS test;")
-            cursor.execute(query)
+        if settings.DATABASES['default'].get("ENGINE") == "djongo":
+            client = MongoClient('localhost', 27017)
+            client.drop_database('test')
+        elif settings.DATABASES['default'].get("ENGINE") == "django.db.backends.postgresql":
+            with connections['default'].cursor() as cursor:
+                query = sql.SQL("DROP DATABASE IF EXISTS test;")
+                cursor.execute(query)
